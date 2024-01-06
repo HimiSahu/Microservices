643ff6c317a4a41392618a319ef36d4d529797e2
HimiSahu <himisahu1112@gmail.com>
2024-01-06 23:00:19 +0530

    100 ConsumerControllerCREATED

---
+++ Consumer/src/main/java/com/webapp2/controller/ConsumerController.java
@@ -0,0 +1,32 @@
+package com.webapp2.controller;
+
+import com.webapp2.entity.Employee;
+import com.webapp2.service.ConsumerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/consumer")
+public class ConsumerController {
+
+    private final ConsumerService consumerService;
+
+  @Autowired
+  public ConsumerController(ConsumerService consumerService) {
+   this.consumerService = consumerService;
+    }
+
+    @GetMapping("/fetch")
+    public String fetchData(){
+        String data = consumerService.fetchDataFromProducer();
+        return data;
+    }
+
+
+    @GetMapping("/employee")
+    public Employee fetchEmployee() {
+        return consumerService.fetchEmployeeFromProducer();
+    }
+}
